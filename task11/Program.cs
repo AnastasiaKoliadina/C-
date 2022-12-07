b4ccd1ddf8b074da5ff4e023a29720ad3957b41a
Anastasia <anastasia7066084@mail.ru>
2022-12-07 18:56:29 +0300

    .\task11\

---
+++ task11/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+//от 1 до N.
+//
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int num = 1;
+Console.WriteLine($"Таблица квадратов числа {N} от 1 до {N}");
+while ( num <= N )
+{
+   double result = num * num;
+   Console.WriteLine(result);
+   num ++;
+
+}
